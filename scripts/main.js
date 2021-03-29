3fc9d03847fa7a9f4e67d3abd8e05780f7caf4ed
moodmister <valentinski6@gmail.com>
2021-03-29 13:15:53 +0300

    Returned to stadia

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ function ScrollTo(point) {
 }
 
 var mymap = L.map('mapid').setView([43.8408650611807, 25.95971783771574], 7);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
   foo: 'bar',
   maxZoom: 20,
   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
